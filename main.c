f2683f741e29152c1dde58acb368049b790c0e0e
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2024-11-12 01:43:46 +0800

    Specify and read data from input

--- main.c
+++ main.c
@@ -1,40 +1,68 @@
-// Include the standard preprocessors directives
+// Include the necessary headers
 #include <stdio.h>
 #include <curl/curl.h>
 
-size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata)
-{
-    fwrite(ptr, size, nmemb, stdout);
-    return size * nmemb;
-}
-
 int main(void)
 {
+
+    /*
+     * emailAddress: The email address to use for authentication (https://www.rfc-editor.org/errata/eid1690)
+     * emailPassword: The password to use for authentication
+     * accountType: The type of account to use for authentication (POP3, IMAP, SMTP)
+     * mailServer: The mail server to use for authentication (https://en.wikipedia.org/wiki/Fully_qualified_domain_name)
+     * url: The combined account type and mail server to use for authentication
+     */
+    char emailAddress[254], emailPassword[128], accountType[5], mailServer[255], url[303];
+
     CURL *curl;
     CURLcode res;
 
     curl = curl_easy_init();
+
     if (curl)
     {
-        curl_easy_setopt(curl, CURLOPT_USERNAME, "");
-        curl_easy_setopt(curl, CURLOPT_PASSWORD, "");
-        curl_easy_setopt(curl, CURLOPT_URL, "");
+        // Prompt the user to enter email account information
+        printf("Email Address: ");
+        scanf("%s", emailAddress);
+        printf("Password: ");
+        scanf("%s", emailPassword);
+        printf("Account Type (POP3 or IMAP): ");
+        scanf("%s", accountType);
+        printf("Mail Server: ");
+        scanf("%s", mailServer);
 
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        // Combine the account type and mail server into a single str with following format: accountType://mailServer
+        sprintf(url, "%s://%s", accountType, mailServer);
 
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        printf("Email Address: %s\n", emailAddress);
+        printf("Password: %s\n", emailPassword);
+        printf("Account Type: %s\n", accountType);
+        printf("Mail Server: %s\n", url);
 
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK)
-        {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        }
-        else
-        {
-            printf("Login successful!\n");
-        }
+        // /*
+        //  * Initialize the CURL object with the necessary options:
+        //  * CURLOPT_USERNAME: The username or email to use for authentication
+        //  * CURLOPT_PASSWORD: The password to use for authentication
+        //  * CURLOPT_URL: The URL of the website to authenticate against
+        //  */
 
-        curl_easy_cleanup(curl);
+        // curl_easy_setopt(curl, CURLOPT_USERNAME, emailAddress);
+        // curl_easy_setopt(curl, CURLOPT_PASSWORD, emailPassword);
+        // curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+        // res = curl_easy_perform(curl);
+        // if (res != CURLE_OK)
+        // {
+        //     fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        // }
+        // else
+        // {
+        //     printf("Login successful!\n");
+        // }
+
+        // curl_easy_cleanup(curl);
     }
     else
     {
